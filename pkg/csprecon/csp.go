4fb35d3a2ad0eb551ec4eec667dd466e4966c446
edoardottt <edoardott@gmail.com>
2022-11-18 19:15:19 +0100

    update

--- pkg/csprecon/csp.go
+++ pkg/csprecon/csp.go
@@ -2,11 +2,14 @@ package csprecon
 
 import (
 	"crypto/tls"
+	"io"
 	"net"
 	"net/http"
 	"regexp"
 	"strings"
 	"time"
+
+	"golang.org/x/net/html"
 )
 
 const (
@@ -29,9 +32,12 @@ func checkCSP(url string, r *regexp.Regexp, client *http.Client) ([]string, erro
 
 	defer resp.Body.Close()
 
-	headerCSP = parseCSPHeader(resp.Header.Get("Content-Security-Policy"), r)
-	if len(headerCSP) != 0 {
-		bodyCSP = parseCSPBody("")
+	headerCSP = parseCSP(resp.Header.Get("Content-Security-Policy"), r)
+	if len(headerCSP) == 0 {
+		bodyCSP, err = parseCSPBody(resp.Body, r)
+		if err != nil {
+			return result, nil
+		}
 	}
 
 	result = append(result, headerCSP...)
@@ -40,7 +46,7 @@ func checkCSP(url string, r *regexp.Regexp, client *http.Client) ([]string, erro
 	return result, nil
 }
 
-func parseCSPHeader(input string, r *regexp.Regexp) []string {
+func parseCSP(input string, r *regexp.Regexp) []string {
 	result := []string{}
 
 	splitted := strings.Split(input, ";")
@@ -57,10 +63,40 @@ func parseCSPHeader(input string, r *regexp.Regexp) []string {
 	return result
 }
 
-func parseCSPBody(input string) []string {
+func parseCSPBody(input io.ReadCloser, r *regexp.Regexp) ([]string, error) {
 	result := []string{}
 
-	return result
+	doc, err := html.Parse(input)
+	if err != nil {
+		return result, err
+	}
+
+	bodyString, err := io.ReadAll(input)
+	if err != nil {
+		return result, err
+	}
+
+	if strings.Contains(string(bodyString), `http-equiv="Content-Security-Policy"`) {
+		// Recursively visit nodes in the parse tree
+		var f func(*html.Node)
+		f = func(n *html.Node) {
+			if n.Data == "meta" {
+				for _, a := range n.Attr {
+					if a.Key == "content" {
+						result = parseCSP(a.Val, r)
+						break
+					}
+				}
+			}
+
+			for c := n.FirstChild; c != nil; c = c.NextSibling {
+				f(c)
+			}
+		}
+		f(doc)
+	}
+
+	return result, nil
 }
 
 func customClient(timeout int) *http.Client {
